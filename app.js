f717dcd79cb4989bd0e858c044029f5c3bbc95d4
Vijayraj-wd <vijayrajck@outlook.com>
2021-10-13 18:47:21 +0530

    Updated

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const addButton = document.querySelector('.add-button');
-const deleteButton = document.querySelector('.delete-button');
 const taskInput = document.querySelector('.input-text');
 const taskArea = document.querySelector('.task-list-container');
 
+let chores = [];
 
 addButton.addEventListener('click', () => {
-    let input = taskInput.value;
+  let input = taskInput.value;
+
+  if (chores.includes(input)) {
+    console.log('Already existed');
+  } else {
+    chores.push(input)
     let template = `<div class="checkmark-container">
                   <input class="checkmark" type="checkbox" unchecked />
                   <span class="checkmark"></span>
@@ -13,14 +18,17 @@ addButton.addEventListener('click', () => {
                 <div class="content-container">
                   <p>${input}</p>
                   <ion-icon
-                    class="delete-button"
+                    class="delete"
                     name="close-outline"
                   ></ion-icon>`
     let element = document.createElement('li');
     element.innerHTML = template;
     taskArea.appendChild(element);
+  }
+  const deleteButton = document.querySelector('.delete')
+  console.log(deleteButton);
+  deleteButton.addEventListener('click', (e) => {
+   
+  })
 })
 
-deleteButton.addEventListener('click', (e) => {
-    taskArea.removeChild
-})
